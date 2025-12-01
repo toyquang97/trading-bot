a5729b962b94335311053b484a7bdd220b3067db
quang ha <hathienquangvn@gmail.com>
2025-12-02 01:16:13 +0700

    update path file, delete unnesscessry file

--- backtest_cp/funding.py
+++
@@ -1,7 +0,0 @@
-# backtest/funding.py
-from include import *
-
-def load_funding_csv(path):
-    df = pd.read_csv(path)
-    df["fundingRate"] = df["fundingRate"].astype(float)
-    return df.set_index("timestamp")["fundingRate"]
--- backtest_cp/testpd.py
+++
@@ -1,9 +0,0 @@
-from strategy import generate_signals   # or wherever your function is
-from strategy import debug_generate_signals  # if you placed wrapper there
-from init import *
-
-file_path = 'BTCUSDT_1m_20251001_0000_to_20251127_2359.csv'
- 
-df = pd.read_csv(file_path)
-debug_out = debug_generate_signals(df, generate_signals)
-print
